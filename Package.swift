d9976d8ccd4394ecfa3655f9aecffef54384d39c
ysnrkdm <euphonictech@gmail.com>
2016-11-09 23:29:58 +0900

    Command line Othello application

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Graphite",
     dependencies: [
-        .Package(url: "https://github.com/ysnrkdm/Graphene", majorVersion: 1, minor: 9)
+        .Package(url: "https://github.com/ysnrkdm/Graphene", majorVersion: 2, minor: 0)
     ]
 )

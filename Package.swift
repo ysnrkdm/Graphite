a6df78d38624253e641090906f8be7c833e25671
ysnrkdm <euphonictech@gmail.com>
2016-11-10 23:48:33 +0900

    Update Graphene 2.1.0

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Graphite",
     dependencies: [
-        .Package(url: "https://github.com/ysnrkdm/Graphene", majorVersion: 2, minor: 0)
+        .Package(url: "https://github.com/ysnrkdm/Graphene", majorVersion: 2, minor: 1)
     ]
 )
